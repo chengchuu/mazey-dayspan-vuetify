9a82f1cdf012d000fee7103f6882f614c05e7855
Cheng <mazeyqian@gmail.com>
2026-07-11 01:17:08 +0800

    fix(page): wrong path

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,2 +1,14 @@
-import { defineConfig } from 'vite'; import vue from '@vitejs/plugin-vue'; import { resolve } from 'node:path'
-export default defineConfig({root:resolve(import.meta.dirname),plugins:[vue()],resolve:{alias:{'mazey-dayspan-vuetify':resolve(import.meta.dirname,'../src/index.ts')}}})
+import { resolve } from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: resolve(import.meta.dirname),
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      'mazey-dayspan-vuetify': resolve(import.meta.dirname, '../src/index.ts'),
+    },
+  },
+})
